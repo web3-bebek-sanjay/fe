7187bdeb43a1fe3eebb1df1ebae5789584d9db56
queredda <mahsaqueredabahjah@mail.ugm.ac.id>
2025-05-04 12:22:23 +0700

    feat: add LoadingContext for managing loading state across the application

---
+++ src/context/LoadingContext.tsx
@@ -0,0 +1,28 @@
+"use client";
+// context/LoadingContext.tsx
+import React, { createContext, useState, useContext, ReactNode } from "react";
+
+type LoadingContextType = {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const LoadingContext = createContext<LoadingContextType | undefined>(undefined);
+
+export const LoadingProvider = ({ children }: { children: ReactNode }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  return (
+    <LoadingContext.Provider value={{ loading, setLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  );
+};
+
+export const useLoading = (): LoadingContextType => {
+  const context = useContext(LoadingContext);
+  if (!context) {
+    throw new Error("useLoading must be used within a LoadingProvider");
+  }
+  return context;
+};
